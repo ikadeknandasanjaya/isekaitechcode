d1bf769ff83faa164d91dcd6c4c58c1245230e5a
ikadeknandasanjaya <kadeksanjaya152@gmail.com>
2022-10-20 19:55:08 +0800

    fix

---
+++ index.js
@@ -0,0 +1,35 @@
+function PageTransition(){
+    var tl = gsap.timeline();
+
+    tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin:"bottom left", stagger: .2})
+    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin:"bottom left", stagger: .1, delay: .1})
+    
+
+}
+
+function contentAnimation()
+{
+    var tl = gsap.timeline();
+    tl.from('.left', {duration: 1.5, translateY: 50, opacity:0})
+    tl.to('img', {clipPath:"polygon(0 0, 100% 0, 100% 100%, 0% 100%"})
+}
+barba.int({
+    sync: true,
+
+    transitions: [{
+        async leave(data) {
+            const done = this.async();
+
+            PageTransition();
+            await Delay(1500);
+            done();
+        },
+
+        async enter(data) {
+            contentAnimation();
+        },
+        async once(data) {
+            contentAnimation();
+        }
+    }]
+})
